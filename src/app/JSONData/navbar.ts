3bd1e442e64293acf41697690e449b028675af97
anika-001 <dream.sweeter.30.09@gmail.com>
2021-10-25 21:23:49 +0530

    initial

---
+++ src/app/JSONData/navbar.ts
@@ -0,0 +1,42 @@
+export let navbar = [
+    {
+        "role":"user",
+        "name":"Mood Tracker",
+        "link":"/dashboard",
+    },
+    {
+        "role":"user",
+        "name":"Forum",
+        "link":"/forum",
+    },
+    {
+        "role":"user",
+        "name":"Study",
+        "link":"/search",
+    },
+    {
+        "role":"user",
+        "name":"News and articles",
+        "link":"/news",
+    },
+    {
+        "role":"user",
+        "name":"Home",
+        "link":"/home",
+    },
+    {
+        "role":"user",
+        "name":"Calmness",
+        "link":"/calm",
+    },
+    {
+        "role":"user",
+        "name":"calendar",
+        "link":"/management",
+    },
+    {
+        "role":"user",
+        "name":"events",
+        "link":"/events",
+    }
+]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCiFuNimhFHTVnJEpWmC44fnmbW08Af-DI",
+    authDomain: "edunation-155bf.firebaseapp.com",
+    projectId: "edunation-155bf",
+    storageBucket: "edunation-155bf.appspot.com",
+    messagingSenderId: "114732340154",
+    appId: "1:114732340154:web:49b2f5df7a8026af054383"
+  }
+};
